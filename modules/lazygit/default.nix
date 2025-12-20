1d4e96ddc4739bba27b9ff611c22b3ae115196af
ANdroxee <anto.dongay123@gmail.com>
2025-12-20 01:29:13 +0100

    modif lazygit

--- modules/lazygit/default.nix
+++ modules/lazygit/default.nix
@@ -7,16 +7,15 @@
     settings = {
       gui.theme = {
         lightTheme = false;
-        activeBorderColor = [ "#f6c177" "bold" ]; # Orange Stray
+        activeBorderColor = [ "#f6c177" "bold" ]; 
         inactiveBorderColor = [ "#a0a8b7" ];
-        optionsTextColor = [ "#9ccfd8" ]; # Bleu cyan
-        selectedLineBgColor = [ "#26233a" ]; # Fond sombre sélectionné
+        optionsTextColor = [ "#9ccfd8" ]; 
+        selectedLineBgColor = [ "#26233a" ]; 
         selectedRangeBgColor = [ "#26233a" ];
       };
     };
   };
   
-  # Alias : tape "lg" pour ouvrir
   home.shellAliases = {
     lg = "lazygit";
   };
